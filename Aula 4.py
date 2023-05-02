8221296890d6cfa51a9f23af44745a1aa62d83ed
Maria Julia <maju.m.valaski@gmail.com>
2023-05-02 15:21:04 -0300

    continue e break

--- Aula 4.py
+++ Aula 4.py
@@ -20,3 +20,29 @@ while (cont<=5):
 media = soma / 5
 print('A média final foi: {}'.format(media))
 
+#validando entrada
+
+x = int(input('Digite um valor maior do que zero: '))
+while (x<=0):
+    x = int(input('Digite um valor maior do que zero: '))
+print('Parabens, você acertou!')
+
+#Break
+print('Bem-vindo ao labirinto das palavras. Escolha uma para tentar sair')
+print('Se acertar a palavra mágica, você sai do labirinto')
+while True:
+    texto = input('')
+    print('Sinto muito, mas ainda não achou a saída')
+    if texto == 'sair':
+        break
+print('Você conseguiu sair do labirinto!')
+
+#Voltando ao inicio do laço com continue
+while True:
+    nome = input('Qual o seu nome?')
+    if (nome != 'Maria'):
+        continue
+    senha = input('Qual a sua senha?')
+    if (senha == 'UnInter'):
+        break
+print('Acesso concedido.')
