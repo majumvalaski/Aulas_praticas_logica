02ef06a32578082c00b2bc2a2344f71ffcd59d29
Maria Julia <maju.m.valaski@gmail.com>
2023-05-02 14:58:32 -0300

    estrutura while

---
+++ Aula 4.py
@@ -0,0 +1,12 @@
+#while
+
+soma = 0
+cont = 1
+
+while (cont<=5):
+    x = float(input("Digite a {}ª nota: ".format(cont)))
+    soma = soma + x
+    cont = cont + 1
+
+media = soma / 5
+print('A média final foi: {}'.format(media))
