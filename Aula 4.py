5e3ab019706c831f6f35d57374ea2f5146baa73d
Maria Julia <maju.m.valaski@gmail.com>
2023-05-02 15:02:13 -0300

    operadores especiais de operaçao

--- Aula 4.py
+++ Aula 4.py
@@ -10,3 +10,13 @@ while (cont<=5):
 
 media = soma / 5
 print('A média final foi: {}'.format(media))
+
+
+while (cont<=5):
+    x = float(input("Digite a {}ª nota: ".format(cont)))
+    soma += x
+    cont += 1
+
+media = soma / 5
+print('A média final foi: {}'.format(media))
+
